6cc30c892a480aec618b61cc39ba9328ec2b7491
sebing18 <sebastianre.developer@gmail.com>
2024-11-20 19:24:16 -0500

    Inicialización Proyecto

---
+++ src/main/java/com/entidadesFinancieras/financieras/FinancierasApplication.java
@@ -0,0 +1,13 @@
+package com.entidadesFinancieras.financieras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancierasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancierasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entidadesFinancieras/financieras/FinancierasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entidadesFinancieras.financieras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancierasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
